881ce5a28339122b147178efd8554b34da018345
Andrew Nesbitt <andrewnez@gmail.com>
2026-02-10 20:50:51 +0000

    Add BuildPURLString fast path that builds string directly without intermediate structs

--- makepurl_test.go
+++ makepurl_test.go
@@ -45,3 +45,71 @@ func TestCleanVersion(t *testing.T) {
 	}
 }
 
+func TestBuildPURLString(t *testing.T) {
+	tests := []struct {
+		name        string
+		ecosystem   string
+		pkgName     string
+		version     string
+		registryURL string
+		want        string
+	}{
+		{"simple npm", "npm", "lodash", "4.17.21", "", "pkg:npm/lodash@4.17.21"},
+		{"scoped npm", "npm", "@babel/core", "7.20.0", "", "pkg:npm/%40babel/core@7.20.0"},  // @ encoded in namespace
+		{"gem", "rubygems", "rails", "7.0.0", "", "pkg:gem/rails@7.0.0"},
+		{"pypi", "pypi", "requests", "2.28.0", "", "pkg:pypi/requests@2.28.0"},
+		{"maven", "maven", "org.apache:commons", "1.0", "", "pkg:maven/org.apache/commons@1.0"},
+		{"golang", "golang", "github.com/foo/bar", "v1.0.0", "", "pkg:golang/github.com/foo/bar@v1.0.0"},
+		{"no version", "npm", "lodash", "", "", "pkg:npm/lodash"},
+		{"with registry", "npm", "lodash", "1.0.0", "https://npm.example.com", "pkg:npm/lodash@1.0.0?repository_url=https:%2F%2Fnpm.example.com"},
+		{"default registry ignored", "npm", "lodash", "1.0.0", "https://registry.npmjs.org", "pkg:npm/lodash@1.0.0"},
+		{"composer", "packagist", "vendor/pkg", "1.0", "", "pkg:composer/vendor/pkg@1.0"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := BuildPURLString(tt.ecosystem, tt.pkgName, tt.version, tt.registryURL)
+			if got != tt.want {
+				t.Errorf("BuildPURLString() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBuildPURLStringMatchesMakePURL(t *testing.T) {
+	// Ensure BuildPURLString produces the same output as the struct-based path
+	cases := []struct {
+		ecosystem   string
+		name        string
+		version     string
+		registryURL string
+	}{
+		{"npm", "lodash", "4.17.21", ""},
+		{"npm", "@babel/core", "7.20.0", ""},
+		{"rubygems", "rails", "7.0.0", ""},
+		{"maven", "org.apache:commons", "1.0", ""},
+		{"golang", "github.com/foo/bar", "v1.0.0", ""},
+		{"packagist", "vendor/pkg", "1.0", ""},
+		{"npm", "lodash", "^1.0.0", ""},
+		{"npm", "pkg", "1.0.0", "https://custom.registry.com"},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.ecosystem+"/"+tt.name, func(t *testing.T) {
+			fast := BuildPURLString(tt.ecosystem, tt.name, tt.version, tt.registryURL)
+
+			purlType := EcosystemToPURLType(tt.ecosystem)
+			cleanVersion := CleanVersion(tt.version, purlType)
+			p := MakePURL(tt.ecosystem, tt.name, cleanVersion)
+			if tt.registryURL != "" && IsNonDefaultRegistry(purlType, tt.registryURL) {
+				p = p.WithQualifier("repository_url", tt.registryURL)
+			}
+			slow := p.String()
+
+			if fast != slow {
+				t.Errorf("mismatch:\n  fast: %q\n  slow: %q", fast, slow)
+			}
+		})
+	}
+}
+
